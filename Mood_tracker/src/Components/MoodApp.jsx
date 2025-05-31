0b8c9bbaca09d71904070938864954520e9e596b
nmdntaa <144686774+nmdntaa@users.noreply.github.com>
2025-05-31 14:06:10 +0700

    Initial commit

--- Mood_tracker/src/Components/MoodApp.jsx
+++ Mood_tracker/src/Components/MoodApp.jsx
@@ -38,3 +38,5 @@ class MoodApp extends React.Component {
     );
   }
 }
+
+export default MoodApp;
--- Mood_tracker/src/index.jsx
+++ Mood_tracker/src/index.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import Mood from './Components/Mood';
 import MoodItem from './Components/MoodItem';
+import MoodApp from './Components/MoodApp';
 
 const room = createRoot(document.getElementById('root'));
 room.render(
   <Mood>
-    <MoodItem />
+    <MoodApp />
   </Mood>
 );
